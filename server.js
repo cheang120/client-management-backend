193e4973f3d6f5e787cd93133a3319563f622683
cheang120 <babycode.2023@gmail.com>
2024-04-08 15:28:24 +0800

    server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(
     cors({
-      origin: ["http://localhost:3002", "https://client-management-app-ten.vercel.app/"],
+      origin: ["http://localhost:3002", "https://client-management-app-ten.vercel.app"],
       credentials: true,
     })
   );
