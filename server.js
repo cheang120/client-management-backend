d42741f804cf05842373528cf9614c4142b06dbb
cheang120 <babycode.2023@gmail.com>
2024-04-08 11:01:16 +0800

    client-management

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(
     cors({
-      origin: ["http://localhost:3002", "https://pinvent-app.vercel.app"],
+      origin: ["http://localhost:3002", "https://client-management.vercel.app"],
       credentials: true,
     })
   );
